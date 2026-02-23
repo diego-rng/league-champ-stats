20176fc7e58c770ff0866be3d6a00846dcf867ba
diego-rng <diego.renova55@gmail.com>
2026-02-23 15:45:21 -0300

    feat: modal funcional com hover das habilidades de cada personagem.

--- src/app/components/modal/modal.ts
+++ src/app/components/modal/modal.ts
@@ -1,20 +1,28 @@
 import { Component, Input, signal, WritableSignal } from "@angular/core";
+import { NgClass } from "@angular/common";
 import { Champion } from "../champion-list/champion-list";
 import { isActive } from "@angular/router";
 
+
 @Component({
   selector: 'champ-modal',
-  templateUrl: './modal.html'
+  templateUrl: './modal.html',
+  imports: [NgClass]
 })
 
 export class ModalComponent {
   @Input() isActive!: WritableSignal<boolean>;
   @Input() champData!: Champion;
 
+  isPassiveHovered = false
+  isQHovered = false
+  isWHovered = false
+  isEHovered = false
+  isRHovered = false
+
 
   disableModal() {
     this.isActive.set(false)
   }
-
 
 }
